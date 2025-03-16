eacf65d23313a9e052cc9b1fbe38145d83886b4f
Jennster <jennygc@telus.net>
2025-03-16 10:44:50 -0700

    update code and put DS_Store in git ignore

--- wordsearch.js
+++ wordsearch.js
@@ -1,37 +1,33 @@
-// receives a 2D array of letters and a word
-// Check to find the word horizontally and vertically
-// Return true if the word is found, and return false if the word is not found
-
-// When the present tests are successful, ask yourself, "Do the current tests cover all the possible cases?"
-// What if the word is written vertically, not horizontally?
-// What about the case where the word matrix is an empty array?
-// write tests for these cases (and any others that you think of)
-// modify the wordSearch function to make those new tests pass.
-
 const wordSearch = (letters, word) => {
-  //we check if the matrix is an empty array and return false if it is
+  // Check if the matrix is an empty array and return false if it is
   if (letters.length === 0) {
     return false;
   }
-  //we check the horiontal arrays by turning them into strings
+
+  // Check the horizontal arrays by turning them into strings
   const horizontalJoin = letters.map((ls) => ls.join(""));
   for (l of horizontalJoin) {
     if (l.includes(word)) {
+      console.log("Word found:", word);
       return true;
     }
   }
-  //we create an array of vertical letters, turn it into a string and check for the word
-  for (let x = 0; x <= letters[0].length; ++x) {
+
+  // Create an array of vertical letters, turn it into a string, and check for the word
+  for (let x = 0; x < letters[0].length; ++x) {
+    // Fix the loop to iterate correctly
     let temp = [];
     for (let y = 0; y < letters.length; ++y) {
       temp.push(letters[y][x]);
     }
-    console.log(temp.join(""));
     if (temp.join("").includes(word)) {
+      console.log("Word found:", word);
       return true;
     }
   }
-  //we return false if the word is not present
+
+  // Return false if the word is not present
+
   return false;
 };
 
@@ -47,14 +43,5 @@ wordSearch(
     ["O", "D", "C", "A", "K", "U", "A", "S"],
     ["E", "Z", "K", "F", "Q", "U", "A", "L"],
   ],
-  "LDLGLBIS8"
+  "QUAL"
 );
-
-module.exports = wordSearch;
-
-// const wordSearch = (letters, word) => {
-//   const horizontalJoin = letters.map((ls) => ls.join(""));
-//   for (l of horizontalJoin) {
-//     if (l.includes(word)) return true;
-//   }
-// };
